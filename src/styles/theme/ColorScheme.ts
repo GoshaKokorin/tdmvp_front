bb070b4edc22f9fff87678a88a05a159a3771816
g.kokorin <g.kokorin@teachbase.ru>
2023-11-06 23:20:22 +0500

    init project

--- src/styles/theme/ColorScheme.ts
+++ src/styles/theme/ColorScheme.ts
@@ -1,10 +1,10 @@
 enum ColorScheme {
   ACCENT = "#487BFF",
   ACCENT_2 = "#2659DC",
-  PRIMARY = "#FFFFFF",
+  PRIMARY = "#3C3C3C",
   SECONDARY = "#7E7E7E",
   TERTIARY = "#D1D1D1",
-  WHITE = "#3C3C3C",
+  WHITE = "#FFFFFF",
   WHITE_TWO = "#F5F5F5",
   GRAY = "#D9D9D9",
   ERROR = "#FF4F4F",
