a174fb79f73b2a1d6e6e0a9552c2944b2fa0c666
g.kokorin <g.kokorin@teachbase.ru>
2023-11-06 23:12:29 +0500

    init project

--- src/styles/theme/ColorScheme.ts
+++ src/styles/theme/ColorScheme.ts
@@ -4,7 +4,7 @@ enum ColorScheme {
   PRIMARY = "#FFFFFF",
   SECONDARY = "#7E7E7E",
   TERTIARY = "#D1D1D1",
-  WHITE = "#FFFFFF",
+  WHITE = "#3C3C3C",
   WHITE_TWO = "#F5F5F5",
   GRAY = "#D9D9D9",
   ERROR = "#FF4F4F",
