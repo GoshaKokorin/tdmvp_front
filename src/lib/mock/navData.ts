50302e1efdad29ddd4fbce5a0a7dabf734b15018
g.kokorin <g.kokorin@teachbase.ru>
2023-11-06 22:26:34 +0500

    init project

--- src/lib/mock/navData.ts
+++ src/lib/mock/navData.ts
@@ -4,5 +4,4 @@ export const navData = [
   { title: "Главная", path: RoutesNamespace.HOME },
   { title: "Каталог", path: RoutesNamespace.CATALOG },
   { title: "О нас", path: RoutesNamespace.ABOUT },
-  { title: "Новости", path: RoutesNamespace.NEWS },
 ];
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -13,35 +13,10 @@ const About = () => {
   return (
     <TitleLayout title="О НАС">
       <div className="padding">
-        <ContainerImageSC>
-          <ImgPage url={aboutPageData.header.image}>
-            <TitleSC>{aboutPageData.header.title}</TitleSC>
-          </ImgPage>
-        </ContainerImageSC>
         <ContainerInfoSC className="container-two">
           <H2SC sx={{ marginBottom: "16px" }} className="content-title">
             {aboutPageData.list.title}
           </H2SC>
-          <TextsList
-            item={aboutPageData.list.items}
-            variant={aboutPageData.list.variant as "number"}
-          />
-          {aboutPageData.desc.map((desc) => (
-            <ParagraphSC key={desc} className="content-margin48">
-              {desc}
-            </ParagraphSC>
-          ))}
-          <figure className="content-margin48">
-            <BottomImageSC
-              className="content-br"
-              src={aboutPageData.bottom.image}
-            />
-            <FigcaptionSC>{aboutPageData.bottom.figcaption}</FigcaptionSC>
-          </figure>
-          {/*<H2SC sx={{ marginBottom: "16px" }}>*/}
-          {/*  {aboutPageData.certificate.title}*/}
-          {/*</H2SC>*/}
-          <Certificate />
         </ContainerInfoSC>
         <Questions />
       </div>
